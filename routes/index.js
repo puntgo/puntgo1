08c45f66acf9f7b137329695a5226372f810b904
212596101 <puneet.tiwari@ge.com>
2017-01-18 11:47:59 +0530

    inital commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET home page. */
+router.get('/render', function(req, res, next) {
+	console.log('1' + path.join(__dirname + '/index.html'));
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
