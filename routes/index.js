52e4df6ba8f00acb1060bf1200e6263b37503a3b
212596101 <puneet.tiwari@ge.com>
2017-01-18 12:30:34 +0530

    add git ignore file

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var path = require('path');
 
 /* GET home page. */
-router.get('/render', function(req, res, next) {
+/*router.get('/render', function(req, res, next) {
 	console.log('1' + path.join(__dirname + '/index.html'));
 	res.sendFile(path.join(__dirname + '/index.html'));
-});
+});*/
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('index', { title: 'Express' });
 });
 
 module.exports = router;
